ac3870fdf02debd83599c254ae6570bb323aa643
Anthony <anthony.doniol@gmail.com>
2018-04-14 14:42:54 +0200

    added teh feedback journey code

---
+++ src/main/java/com/devopsbuddy/config/ProductionConfig.java
@@ -0,0 +1,23 @@
+package com.devopsbuddy.config;
+
+import com.devopsbuddy.backend.service.EmailService;
+import com.devopsbuddy.backend.service.SmtpEmailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by tedonema on 21/03/2016.
+ */
+@Configuration
+@Profile("prod")
+@PropertySource("file:C:/Users/Anthony/Desktop/dev/Udemy_devopsbuddy/properties/application-prod.properties")
+public class ProductionConfig {
+
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
+    }
+
+}
